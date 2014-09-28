e0689ac4b98b52c2c403d0d7fabf722972be749d
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-09-27 21:05:48 -0400

    Update SMSActivity.java

--- Android_SDK_ue/Demos/src/main/src/com/estimote/examples/demos/SMSActivity.java
+++ Android_SDK_ue/Demos/src/main/src/com/estimote/examples/demos/SMSActivity.java
@@ -32,32 +32,5 @@ public class SMSActivity extends Activity {
 				Toast.LENGTH_LONG).show();
 			e.printStackTrace();
 		  }
- 
-//		buttonSend = (Button) findViewById(R.id.buttonSend);
-//		textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
-//		textSMS = (EditText) findViewById(R.id.editTextSMS);
- 
-//		buttonSend.setOnClickListener(new OnClickListener() {
-// 
-//			@Override
-//			public void onClick(View v) {
-// 
-//			  String phoneNo = textPhoneNo.getText().toString();
-//			  String sms = textSMS.getText().toString();
-// 
-//			  try {
-//				SmsManager smsManager = SmsManager.getDefault();
-//				smsManager.sendTextMessage(phoneNo, null, sms, null, null);
-//				Toast.makeText(getApplicationContext(), "SMS Sent!",
-//							Toast.LENGTH_LONG).show();
-//			  } catch (Exception e) {
-//				Toast.makeText(getApplicationContext(),
-//					"SMS faild, please try again later!",
-//					Toast.LENGTH_LONG).show();
-//				e.printStackTrace();
-//			  }
-// 
-//			}
-//		});
 	}
 }
